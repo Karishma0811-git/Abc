73d21c65f84839eaa5cf5ce6573fd3bf9c470427
CuCumber28 <arorakarishma0811@gmail.com>
2024-08-09 14:12:13 +0530

    second commit

--- src/test/java/abc.java
+++ src/test/java/abc.java
@@ -2,5 +2,6 @@ public class abc {
     public static void main(String[] args) {
         System.out.println("Hi welcome to new project");
         System.out.println("add new line in github");
+        System.out.println("commit and push to remote of abc");
     }
 }
